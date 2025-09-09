3cfb1e2bdd36e909bb92421ee9be5dacbc615886
aldogg10 <179862@upslp.edu.mx>
2025-09-08 19:13:01 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,27 +1,26 @@
-CREATE TABLE estudiante (
+CREATE TABLE Estudiante (
   id_estudiante INTEGER PRIMARY KEY,
   nombre TEXT NOT NULL,
   apellido TEXT NOT NULL
 );
 
-CREATE TABLE maestros (
+CREATE TABLE Maestros (
   id_profesor INTEGER PRIMARY KEY,
   nombre_profesor TEXT NOT NULL,
   apellido_profesor TEXT NOT NULL,
   departamento TEXT NOT NULL
 );
 
-CREATE TABLE grupo (
+CREATE TABLE Grupo (
   id_curso varchar(15),
   periodo varchar(15),
   id_profesor INT REFERENCES Maestros(id_profesor) NOT NULL,
   nombre_curso TEXT NOT NULL,
-  creditos TEXT NOT NULL,
   PRIMARY KEY (id_curso, periodo)
 );
 
 
-CREATE TABLE inscripcion (
+CREATE TABLE Inscripcion (
   id_estudiante INT REFERENCES Estudiante(id_estudiante) NOT NULL,
   id_curso varchar(15),
   periodo varchar(15),
@@ -29,7 +28,7 @@ CREATE TABLE inscripcion (
   FOREIGN KEY (id_curso, periodo) REFERENCES Grupo(id_curso, periodo)
 );
 
-CREATE TABLE asistencia (
+CREATE TABLE Asistencia (
   id_estudiante INT REFERENCES Estudiante(id_estudiante) NOT NULL,
   id_curso varchar(15) NOT NULL,
   periodo varchar(15) NOT NULL,
